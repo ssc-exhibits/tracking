e7d77de6a92b3bf5dc5a939800b09950bb9f7b68
ssc-exhibits <rholota@sasksciencecentre.com>
2025-02-14 13:36:51 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ExhibitChecklist from "./ExhibitChecklist";
+
+function App() {
+  return (
+    <div>
+      <h1>Exhibit Maintenance Tracker</h1>
+      <ExhibitChecklist />
+    </div>
+  );
+}
+
+export default App;
